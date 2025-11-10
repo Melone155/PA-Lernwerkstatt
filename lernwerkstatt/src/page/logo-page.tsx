31b523a120210e7418c08e4931ee79f9336c71b7
Melone155 <maximilianwolf04@gmail.com>
2025-11-10 13:29:26 +0100

    Add Logo Page

--- lernwerkstatt/src/page/logo-page.tsx
+++ lernwerkstatt/src/page/logo-page.tsx
@@ -2,7 +2,7 @@ import { Gamepad2 } from "lucide-react";
 
 export default function LogoPage() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br bg-black">
+        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br bg-[#075033]">
             {/* Logo */}
             <div className="flex items-center space-x-4">
                 <Gamepad2 className="h-24 w-24 text-purple-400 drop-shadow-lg" />
